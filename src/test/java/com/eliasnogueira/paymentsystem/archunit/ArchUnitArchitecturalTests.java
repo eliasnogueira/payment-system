4b6bc723b63ec5771d65b2b0f11fc8dbf63f23e4
eliasnogueira <elias.nogueira@gmail.com>
2025-03-06 20:06:34 +0100

    chore: cleanup

--- src/test/java/com/eliasnogueira/paymentsystem/archunit/ArchUnitArchitecturalTests.java
+++ src/test/java/com/eliasnogueira/paymentsystem/archunit/ArchUnitArchitecturalTests.java
@@ -2,63 +2,45 @@ package com.eliasnogueira.paymentsystem.archunit;
 
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static com.tngtech.archunit.core.importer.ImportOption.Predefined.DO_NOT_INCLUDE_TESTS;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noFields;
 import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
 
 class ArchUnitArchitecturalTests {
 
-    private final JavaClasses importedClasses = new ClassFileImporter().withImportOption(DO_NOT_INCLUDE_TESTS)
-            .importPackages("com.eliasnogueira.paymentsystem");
+    private final JavaClasses importedClasses = new ClassFileImporter().withImportOption(DO_NOT_INCLUDE_TESTS).importPackages("com.eliasnogueira.paymentsystem");
 
     @Test
     void controllersShouldBeNamedProperly() {
-        classes().that().resideInAPackage("..controller..")
-                .should().haveSimpleNameEndingWith("Controller").check(importedClasses);
+        classes().that().resideInAPackage("..controller..").should().haveSimpleNameEndingWith("Controller").check(importedClasses);
     }
 
     @Test
     void repositoriesShouldBeNamedProperly() {
-        classes().that().resideInAPackage("..repository..")
-                .should().haveSimpleNameEndingWith("Repository").check(importedClasses);
+        classes().that().resideInAPackage("..repository..").should().haveSimpleNameEndingWith("Repository").check(importedClasses);
     }
 
     @Test
     void servicesShouldBeNamedProperly() {
-        classes().that().resideInAPackage("..service..")
-                .should().haveSimpleNameEndingWith("Service").check(importedClasses);
+        classes().that().resideInAPackage("..service..").should().haveSimpleNameEndingWith("Service").check(importedClasses);
     }
 
     @Test
     void serviceClassesShouldOnlyBeAccessedByController() {
-        classes().that().resideInAPackage("..service..").should().onlyBeAccessed()
-                .byAnyPackage("..service..", "..controller").check(importedClasses);
+        classes().that().resideInAPackage("..service..").should().onlyBeAccessed().byAnyPackage("..service..", "..controller").check(importedClasses);
     }
 
     @Test
     void layeredArchitectureShouldBeFollowed() {
-        layeredArchitecture().consideringOnlyDependenciesInLayers()
-                .layer("Controller").definedBy("com.eliasnogueira.paymentsystem.controller")
-                .layer("Repository").definedBy("com.eliasnogueira.paymentsystem.repository")
-                .layer("Service").definedBy("com.eliasnogueira.paymentsystem.service")
-                .whereLayer("Controller").mayNotBeAccessedByAnyLayer()
-                .whereLayer("Repository").mayOnlyBeAccessedByLayers("Service")
-                .whereLayer("Service").mayOnlyBeAccessedByLayers("Controller")
-                .check(importedClasses);
-    }
-
-    @Test
-    void noDisabledTests() {
-        noClasses().should().beAnnotatedWith(Disabled.class).check(importedClasses);
+        layeredArchitecture().consideringOnlyDependenciesInLayers().layer("Controller").definedBy("com.eliasnogueira.paymentsystem.controller").layer("Repository").definedBy("com.eliasnogueira.paymentsystem.repository").layer("Service").definedBy("com.eliasnogueira.paymentsystem.service").whereLayer("Controller").mayNotBeAccessedByAnyLayer().whereLayer("Repository").mayOnlyBeAccessedByLayers("Service").whereLayer("Service").mayOnlyBeAccessedByLayers("Controller").check(importedClasses);
     }
 
     @Test
     void noAutowired() {
-        noClasses().should().beAnnotatedWith(Autowired.class).check(importedClasses);
+        noFields().should().beAnnotatedWith(Autowired.class).check(importedClasses);
     }
 }
